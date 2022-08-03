181b013272b46bd16f2f4bc5d0d96ed80457f64d
Russ Williams <ruwill@microsoft.com>
2022-08-03 14:53:43 -0400

    Reverting config files to remove config settings that where checked in by mistake

--- agent-portal/src/settings/appsettings.ts
+++ agent-portal/src/settings/appsettings.ts
@@ -1,6 +1,6 @@
 // App settings for the agent-portal
 export const appsettings = {
-    agentHubBaseAddress: 'http://localhost:7071',
-    webPusSubConnectionString: 'Endpoint=https://acsagenthubrhwwps.webpubsub.azure.com;AccessKey=1uzqaDhRx4xCesM8Qg9E5hZU44Y0OfybHaq6Hj0m0qA=;Version=1.0;',
+    agentHubBaseAddress: '',
+    webPusSubConnectionString: '',
     webPubSubHubName: 'refreshConversations'
 };
